b4e0baa6afd0522e8ca453544f01bec8f9a3ffdc
kodali10 <rkodali10@outlook.com>
2017-08-31 18:42:53 -0500

    Roll Dice button functionality

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, diceDisplay = document.querySelector('.dice');
 
 function init() {
     scores = [0, 0];
     roundScore = 0;
     activePlayer = 0;
 
-    document.querySelector('.dice').style.display = 'none';
+    diceDisplay.style.display = 'none';
 
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
@@ -14,4 +14,19 @@ function init() {
 }
 
 init();
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    var dice = Math.floor(Math.random() * 6) + 1;
+    diceDisplay.style.display = 'block';
+    diceDisplay.src = 'dice-' + dice + '.png'
+
+    if (dice !== 1) {
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    }
+    else {
+
+
+    }
+});
 
