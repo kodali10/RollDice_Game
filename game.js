37c32070223c7de572393b8ce3595724400fc34b
kodali10 <rkodali10@outlook.com>
2017-08-31 18:24:37 -0500

    init function: resets score board

---
+++ game.js
@@ -0,0 +1,17 @@
+var scores, roundScore, activePlayer;
+
+function init() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+}
+
+init();
+
